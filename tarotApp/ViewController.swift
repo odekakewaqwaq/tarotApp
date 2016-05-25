242904899788763ee1c082d5429cf44d4c493558
odekakewaqwaq <odekakewaqwaq@gmail.com>
2016-05-26 06:25:50 +0900

    Initial Commit

---
+++ tarotApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tarotApp
+//
+//  Created by 永井 伸枝 on 2016/05/26.
+//  Copyright © 2016年 nobue.nagai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
