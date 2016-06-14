27c2de0cb232aadc75563d01433c1c31630073b0
odekakewaqwaq <odekakewaqwaq@gmail.com>
2016-06-15 06:40:28 +0900

    captionのplistを追加

--- tarotApp/CaptionViewController.swift
+++ tarotApp/CaptionViewController.swift
@@ -15,6 +15,28 @@ class CaptionViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initUIGesture()
+        initCaption()
+    }
+    
+    func initUIGesture(){
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "handleTap:")
+        self.view.addGestureRecognizer(tapGesture)
+        
+        let swipeDownGesture: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: "handleSwipe:")
+        swipeDownGesture.numberOfTouchesRequired = 1  // number of fingers
+        swipeDownGesture.direction = UISwipeGestureRecognizerDirection.Down
+        self.view.addGestureRecognizer(swipeDownGesture)
+        
+        let swipeUpGesture:UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: "handleSwipe:")
+        swipeUpGesture.numberOfTouchesRequired = 1
+        swipeUpGesture.direction = UISwipeGestureRecognizerDirection.Up
+        self.view.addGestureRecognizer(swipeUpGesture)
+        
+    }
+    
+    func initCaption(){
+        label.hidden = true
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -26,6 +48,21 @@ class CaptionViewController: UIViewController {
         let image =  UIImage(named: str)
         imageView.image = image
     }
+    
+    func handleTap(sender: UITapGestureRecognizer){
+        print("Tapped!")
+        if label.hidden {
+            label.hidden = false
+        } else{
+            label.hidden = true
+        }
+    }
+    
+    func handleSwipe(sender: UITapGestureRecognizer){
+        print("swiped!")
+        performSegueWithIdentifier("fromCaptionToCollection", sender: nil)
+    }
+    //fromCaptionToCollection
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
