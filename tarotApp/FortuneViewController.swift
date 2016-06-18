85e2a7299cc96541fea3f92fe0240d3760baf8d6
odekakewaqwaq <odekakewaqwaq@gmail.com>
2016-06-18 18:51:38 +0900

    占い中のビューの背景を回転

--- tarotApp/FortuneViewController.swift
+++ tarotApp/FortuneViewController.swift
@@ -9,21 +9,26 @@
 import UIKit
 
 class FortuneViewController: UIViewController {
+    @IBOutlet weak var backGroundView: UIImageView!
     @IBOutlet weak var titleView: UIView!
+    @IBOutlet weak var guruguruFaceView: UIImageView!
+    
     var titleImageArray: Array<UIImage> = []
     var titleViewArray: Array<UIImageView> = []
+    let guruguruFaceAngle = CGFloat(M_PI * 0.3)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         initTitleImageArray()
         initTitleView()
-        
+        initGuruGuruFace()
         // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(true)
         animateTitleView()
+        rotateAnim()
     }
 
     func initTitleImageArray(){
@@ -44,7 +49,13 @@ class FortuneViewController: UIViewController {
         }
     }
     
+    func initGuruGuruFace(){
+        self.guruguruFaceView.transform = CGAffineTransformMakeRotation(guruguruFaceAngle * -0.7)
+    }
+
+    
     func animateTitleView(){
+        let angle = self.guruguruFaceAngle
         for (i,a) in titleViewArray.enumerate() {
         let num = Double(i)
         UIView.animateWithDuration(0.5, delay: 0.1 * num, options:[.Repeat, .Autoreverse] , animations: {
@@ -52,7 +63,22 @@ class FortuneViewController: UIViewController {
             self.titleViewArray[i].center = CGPoint(x: 0, y: 100)
             }
             , completion: nil)
+        }
+        
+        UIView.animateWithDuration(2, delay: 0, options: [.Repeat, .Autoreverse], animations: {
+            () -> Void in
+            self.guruguruFaceView.transform = CGAffineTransformMakeRotation(angle)
+            }, completion: nil)
     }
+    
+    func rotateAnim() {
+        
+        UIView.animateWithDuration(2, animations: {
+            let angle = CGFloat(M_PI * 1)
+            self.backGroundView.transform = CGAffineTransformRotate(self.backGroundView.transform, angle)
+            }, completion: { (flag) in
+                self.rotateAnim()
+        })
     }
     
     override func didReceiveMemoryWarning() {
